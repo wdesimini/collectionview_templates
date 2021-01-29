65bb4fa120ae3ae7b4031c95ace7a6ddd33e902a
wdesimini <wdesimini8@gmail.com>
2021-01-28 18:00:01 -0800

    Initial Commit

---
+++ collectionview_templates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionview_templates
+//
+//  Created by Wilson Desimini on 1/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
