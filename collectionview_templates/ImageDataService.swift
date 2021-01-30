d13ab0f7b6ea55b4520bf27bae9464d3f524bb66
wdesimini <wdesimini8@gmail.com>
2021-01-29 16:40:16 -0800

    switch image order

--- collectionview_templates/ImageDataService.swift
+++ collectionview_templates/ImageDataService.swift
@@ -11,8 +11,8 @@ struct ImageDataService {
     private static let imageNames = [
         "gleb-albovsky-wUipvJNqgzw-unsplash",
         "joel-protasio-ZTaixh3Yc8s-unsplash",
-        "joshua-lawrence-0YF6r6NcIQo-unsplash",
         "kym-mackinnon-f3ITqoZOP6E-unsplash",
+        "joshua-lawrence-0YF6r6NcIQo-unsplash",
         "mike-palmowski-Aqo0PiobYxk-unsplash",
         "nikhil-singh-zzAyexANejU-unsplash",
         "philip-graves-QeVzrg2eE1Y-unsplash",
